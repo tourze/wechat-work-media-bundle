1bb7f8e485acc64ceaae1361e911cacc78165c32
github-ci <you@example.com>
2025-05-08 08:47:49 +0000

    fix

--- src/Entity/TempMedia.php
+++ src/Entity/TempMedia.php
@@ -28,7 +28,6 @@ class TempMedia
 {
     #[ExportColumn]
     #[ListColumn(order: -1, sorter: true)]
-    #[Groups(['restful_read', 'admin_curd', 'recursive_view', 'api_tree'])]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
